20745a4a765f47497cbc74416e9566fbc12f1b0f
Christopher Dorst <chris@dorst.email>
2017-12-27 17:22:52 -0600

    Adding DotnetCommandRunner

---
+++ DevOps.Build.Functions.RunDotnetCommand/DotnetCommandRunner.cs
@@ -0,0 +1,15 @@
+﻿using static Common.Functions.RunCommandLineTool.CommandLineToolRunner;
+
+namespace DevOps.Build.Functions.RunDotnetCommand
+{
+    public static class DotnetCommandRunner
+    {
+        private const string dotnet = nameof(dotnet);
+
+        public static void Dotnet(string directory, string command, params string[] arguments)
+            => Prompt(directory,
+                tool: dotnet,
+                command,
+                arguments);
+    }
+}
